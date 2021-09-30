b18618dba9386e967c4450315f297e000e2f7cf4
mariyapoly <poly.craft2141@gmail.com>
2021-09-30 23:21:50 +0600

    added inventory component

---
+++ src/components/Inventory/Inventry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventry = () => {
+    return (
+        <div>
+            <h1>this is Inventry pages</h1>
+        </div>
+    );
+};
+
+export default Inventry;
